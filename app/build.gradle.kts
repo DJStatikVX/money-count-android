6463f070e955739e4b803a86b15182aa7cf7f418
DJStatikVX <samuel.rodriguez.dev@outlook.es>
2026-05-02 18:32:51 +0200

    chore: enable release resources shrinking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ configure<ApplicationExtension> {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
